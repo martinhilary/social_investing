ae63b6cade5a4befb1aa3e56f6f790ed047ed78f
martinhilary <martinhilary96@gmail.com>
2018-08-11 15:21:57 +0800

    Created the stock ticker

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,16 @@
 class HomeController < ApplicationController
   def index
+  	if params[:id] == ""
+  		@nothing = "Hey, you forgot to enter a ticker symbol"
+  	elsif
+  		if params[:id]
+		@stock= StockQuote::Stock.quote(params[:id])
+		end
+  	end
   end
 
   def about
   end
-  
+
+
 end
